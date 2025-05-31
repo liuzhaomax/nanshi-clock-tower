1b46c6d41f847cac065b435942d0f9e082d00478
LIUZHAO <liuzhaomax@163.com>
2025-05-31 21:07:22 +0800

    init

---
+++ client/nct/src/pages/index/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text, Image } from 'remax/wechat';
+import styles from './index.css';
+import imgNct from '../../asset/img/nct.jpg';
+
+export default () => {
+  return (
+    <View className={styles.app}>
+      <View className={styles.header}>
+        <Image
+          src={imgNct}
+          className={styles.logo}
+          alt="logo"
+        />
+        <View className={styles.text}>
+          南市中楼 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
+          开始
+        </View>
+      </View>
+    </View>
+  );
+};
